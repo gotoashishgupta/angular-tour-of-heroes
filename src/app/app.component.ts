c47aa49d1eb5a438ce84be46955669f71be26410
Ashish Gupta <ashish.gupta5@ge.com>
2017-08-22 12:08:57 +0530

    FEAT+. Using polymer inside angular with two way data-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,16 @@ export class Hero {
   <h1>{{title}}</h1>
   <h2>{{hero.name}} details!</h2>
   <div><label>id: </label>{{hero.id}}</div>
-  <div><label>name: </label><input [(ngModel)]="hero.name" placeholder="name"></div>
+  <div>
+  <!--
+    @NOTE: If the app imported collection modules, such as PaperElementsModule,
+    [emitChanges] and [ironControl] must not be added to elements that the collection provides selectors for.
+      * Add the [emitChanges] directive to all custom elements using two-way data binding.
+      * Add [ironControl] to control elements that should work in Angular forms.
+  -->
+    <paper-input label="Name" name="name" required [(ngModel)]="hero.name" placeholder="Name"></paper-input>
+    <input [(ngModel)]="hero.name" placeholder="name">
+  </div>
   `,
   styleUrls: ['./app.component.css']
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
+import { PolymerModule } from '@codebakery/origami';
+/**
+ * @Optional: There are many collections to import, such as iron, paper, and gold elements
+ */
+import { IronElementsModule, PaperElementsModule } from '@codebakery/origami/lib/collections'; // Optional
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +14,15 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+
+    PolymerModule.forRoot(),
+    // Optional polymer modules to help reduce markup complexity
+    IronElementsModule,
+    PaperElementsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
