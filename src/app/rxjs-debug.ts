34ab5aca042fdb211b0111572052c2679b5861ee
Ashish Gupta <ashish.gupta5@ge.com>
2017-08-30 12:01:13 +0530

    FEAT+. Adding debug to RxJs/Observables

---
+++ src/app/rxjs-debug.ts
@@ -0,0 +1,31 @@
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/do';
+import { Observable } from 'rxjs/Observable';
+
+const debuggerOn = true;
+
+Observable.prototype.debug = function (message: string) {
+  return this.do(
+    function (next) {
+      if (debuggerOn) {
+        console.log(message, next);
+      }
+    },
+    function (err) {
+      if (debuggerOn) {
+        console.error('ERROR >>> ', message, err);
+      }
+    },
+    function () {
+      if (debuggerOn) {
+        console.log('Completed.');
+      }
+    }
+  );
+};
+declare module 'rxjs/Observable' {
+  interface Observable<T> {
+    debug: (...any) => Observable<T>;
+  }
+}
