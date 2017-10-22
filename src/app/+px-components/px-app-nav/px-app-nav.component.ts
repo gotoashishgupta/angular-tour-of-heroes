671715b111766e65311e87715fd9e9eeb1c3c107
Ashish Gupta <gotoashishgupta@gmail.com>
2017-10-22 17:58:54 +0530

    WIP angular universal implementation

--- src/app/+px-components/px-app-nav/px-app-nav.component.ts
+++ src/app/+px-components/px-app-nav/px-app-nav.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PxAppNavComponent implements OnInit {
   public items;
+  public selectedRoute;
   constructor() { }
 
   public ngOnInit() {
