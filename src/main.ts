bd7c844437bb9e22f9cf93f28743f0572d2c62bf
Ashish Gupta <gotoashishgupta@gmail.com>
2017-11-26 09:50:42 +0530

    chore: enable long stack trace using zone.js

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+} else {
+  require('zone.js/dist/long-stack-trace-zone');
 }
 
 document.addEventListener('DOMContentLoaded', () => {
